eea07019623f0528014a90c01369a738d33600a8
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 10:02:42 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -4,7 +4,8 @@
 (cl:in-package #:place-utils_tests)
 
 (define-test "featured examples"
-  (is equal
+  (is equal '((nil (0 10 2) 10)
+              "(0 1 2)")
       (let* ((my-list (list 0 1 2))
              (my-other-list my-list)
              (output (make-array 0 :element-type 'character :fill-pointer t :adjustable t)))
@@ -13,24 +14,20 @@
                   (setf my-list nil second 8)
                   (incf second 2)
                   (list my-list my-other-list second))
-                output)))
-      '((nil (0 10 2) 10)
-        "(0 1 2)"))
+                output))))
   (flet ((double (number)
            (* number 2)))
-    (is equal
+    (is equal '(4 nil)
         (let ((a 2) (b 8))
           (updatef (values a b) #'double)
-          (list a b))
-        '(4 nil))
-    (is equal
+          (list a b)))
+    (is equal '(6 -8)
         (let ((a 2) (b 8))
           (updatef a #'1+
                    a #'double
                    b #'-)
-          (list a b))
-        '(6 -8))
-    (is equalp
+          (list a b)))
+    (is equalp '(#(1 4) 3)
         (let ((a (vector 1 2))
               (printcount 0))
           (flet ((fakeprint (value)
@@ -38,9 +35,8 @@
                    value))
             (updatef (aref (fakeprint a) (fakeprint 1))
                      (fakeprint #'double))
-            (list a printcount)))
-        '(#(1 4) 3)))
-  (is equal
+            (list a printcount)))))
+  (is equal '(25 100 (25 100))
       (flet ((bulkf-transfer (quantity source destination)
                (values (- source quantity)
                        (+ destination quantity))))
@@ -53,9 +49,8 @@
               (transferf 10
                          (first account-amounts)
                          (second account-amounts))
-              account-amounts))))
-      '(25 100 (25 100)))
-  (is equal
+              account-amounts)))))
+  (is equal '(0 0 (nil 0 nil))
       (flet ((bulkf-init (value number-of-places)
                (values-list (make-list number-of-places
                                        :initial-element value))))
@@ -65,8 +60,7 @@
                              :write ,@places)))
           (let (a b (c (make-list 3 :initial-element nil)))
             (initf 0 a b (second c))
-            (list a b c))))
-      '(0 0 (nil 0 nil)))
+            (list a b c)))))
   (flet ((bulkf-spread (spread-function sum-function
                                         &rest place-values)
            (values-list
@@ -79,26 +73,26 @@
     (macrolet ((spreadf (spread-function sum-function &rest places)
                  `(bulkf #'bulkf-spread :pass ,spread-function ,sum-function
                          :access ,@places)))
-      (is equal
+      (is equal '(11 (11 11 20))
           (let ((a 5) (b (list 10 18 20)))
             (spreadf #'/ #'+ a (first b) (second b))
-            (list a b))
-          '(11 (11 11 20)))
-      (is equal
+            (list a b)))
+      (is equal '(512 (512 512 512))
           (let ((a 2) (b (list 2 4 8)))
             (spreadf #'* #'* a (first b) (second b) (third b))
-            (list a b))
-          '(512 (512 512 512)))))
-  (is equal
+            (list a b)))))
+  (is equal '((1 6 16) 1 6 (10 16))
       (flet ((bulkf-map (function &rest place-values)
                (values-list (mapcar function place-values))))
         (macrolet ((mapf (function &rest places)
                      `(bulkf #'bulkf-map :pass ,function :access ,@places)))
           (let ((a 0) (b 5) (c (list 10 15)))
             (list (multiple-value-list (mapf #'1+ a b (second c)))
-                  a b c))))
-      '((1 6 16) 1 6 (10 16)))
-  (is equal
+                  a b c)))))
+  (is equal '((:INITIAL-ASSETS (:PAINTINGS :COLLECTION) 20000 NIL :RANDOM-STUFF 400)
+              NIL
+              0
+              (:NOTHING-VALUABLE NIL 0))
       (flet ((bulkf-steal (sum-function steal-function
                                         initial-assets &rest target-assets)
                (let (stolen leftovers)
@@ -125,22 +119,17 @@
                           (values nil assets)
                           (values assets (if (numberp assets) 0 nil))))
                     cave museum house (first triplex) (second triplex) (third triplex))
-            (list cave museum house triplex))))
-      '((:INITIAL-ASSETS (:PAINTINGS :COLLECTION) 20000 NIL :RANDOM-STUFF 400)
-        NIL
-        0
-        (:NOTHING-VALUABLE NIL 0)))
+            (list cave museum house triplex)))))
   (flet ((test (initial-value expected-result)
-           (is equal
+           (is equal expected-result
                (let ((cache initial-value)
                      (output (make-array 0 :element-type 'character :fill-pointer t :adjustable t)))
                  (with-output-to-string (*standard-output* output)
                    (incf (cachef nil cache 0 :test #'numberp) (princ (+ 5 2)))
-                   (list cache output)))
-               expected-result)))
+                   (list cache output))))))
     (test "cached-string" '(7 "7"))
     (test 20 '(27 "7")))
-  (is equal
+  (is equal '((:full :computed-value) "(:EMPTY :PLACEHOLDER)")
       (let ((values (list :empty :placeholder))
             (output (make-array 0 :element-type 'character :fill-pointer t :adjustable t)))
         (with-output-to-string (*standard-output* output)
@@ -151,20 +140,16 @@
                             (:full t)
                             (:empty nil)))
                   :new-cachedp :full)
-          (list values output)))
-      '((:full :computed-value) "(:EMPTY :PLACEHOLDER)"))
-  (is equal
+          (list values output))))
+  (is equal '(5 7)
       (let ((a 5))
         (list (incf (oldf a) 2)
-              a))
-      '(5 7))
-  (is equal
+              a)))
+  (is equal '(5 10)
       (let ((a 5))
         (list (setf (oldf a) 10)
-              a))
-      '(5 10))
-  (is equal
+              a)))
+  (is equal '((1 2 3) (0 1 2 3))
       (let ((list '(1 2 3)))
         (list (push 0 (oldf list))
-              list))
-      '((1 2 3) (0 1 2 3))))
+              list))))
